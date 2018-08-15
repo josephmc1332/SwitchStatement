235c81acda174e41e57d1563e02ecd9ece3be536
Josephmc1332 <Josephmc1332@gmail.com>
2018-08-15 16:03:28 -0400

    switch

--- switch statements/Program.cs
+++ switch statements/Program.cs
@@ -10,6 +10,46 @@ namespace switch_statements
     {
         static void Main(string[] args)
         {
+            //switch statements
+            string[] daysOfTheWeek = { "sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday" }; 
+            // inital switch statement using array specififying the fifth seat.
+            switch (daysOfTheWeek [5])
+            {
+                //standard switch format. each case to display text using console.writeline. & break to prevent leak
+                case "sunday":
+                    Console.WriteLine("day one");
+                    break;
+
+                case "monday":
+                    Console.WriteLine("day two");
+                    break;
+
+                case "tuesday":
+                    Console.WriteLine("day three");
+                    break;
+
+                case "wednesday":
+                    Console.WriteLine("day four");
+                    break;
+
+                case "thursay":
+                    Console.WriteLine("day five");
+                    break;
+
+                case "friday":
+                    Console.WriteLine("day six");
+                    break;
+
+                case "saturday":
+                    Console.WriteLine("day seven");
+                    break;
+
+                //default statement to prevent crash from requests outside of array
+                default:
+                    Console.WriteLine("do not recognize your answer");
+                    break;
+            }
+
         }
     }
 }
